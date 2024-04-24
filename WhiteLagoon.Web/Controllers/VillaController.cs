bcc94b502ea65c91fd6953822b24e173c28b1831
Diontae Ford <forddt@dot.state.al.us>
2024-04-24 15:41:13 -0500

    Temp Data Notifications

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -37,6 +37,8 @@ namespace WhiteLagoon.Web.Controllers
                 _db.Villas.Add(obj);
                 _db.SaveChanges();
 
+                TempData["success"] = "The villa has been created successfully.";
+
                 return RedirectToAction("Index");
             }
 
@@ -62,6 +64,8 @@ namespace WhiteLagoon.Web.Controllers
             {
                 _db.Villas.Update(obj);
                 _db.SaveChanges();
+
+                TempData["success"] = "The villa has been updated successfully.";
 
                 return RedirectToAction("Index");
             }
@@ -91,8 +95,12 @@ namespace WhiteLagoon.Web.Controllers
                 _db.Villas.Remove(objFromDb);
                 _db.SaveChanges();
 
+                TempData["success"] = "The villa has been deleted successfully.";
+
                 return RedirectToAction("Index");
             }
+
+            TempData["error"] = "The villa could not be deleted.";
 
             return View(obj);
         }
