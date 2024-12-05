422b80a2f2bfc3bd59d6252ce7415f7e33c2e44b
Diontae Ford <forddt@dot.state.al.us>
2024-12-05 16:49:51 -0600

    Create Amenity Model

--- WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -10,13 +10,14 @@ namespace WhiteLagoon.Infrastructure.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
         public DbSet<Villa> Villas { get; set; }
         public DbSet<VillaNumber> VillaNumbers { get; set; }
+        public DbSet<Amenity> Amenities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -25,33 +26,33 @@ namespace WhiteLagoon.Infrastructure.Data
             modelBuilder.Entity<Villa>().HasData(
                 new Villa
                 {
-                Id = 1,
-                Name = "Royal Villa",
-                Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                ImageUrl = "https://placehold.co/600x400",
-                Occupancy = 4,
-                Price = 200,
-                Sqft = 550,
+                    Id = 1,
+                    Name = "Royal Villa",
+                    Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    ImageUrl = "https://placehold.co/600x400",
+                    Occupancy = 4,
+                    Price = 200,
+                    Sqft = 550,
                 },
                 new Villa
                 {
-                Id = 2,
-                Name = "Premium Pool Villa",
-                Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                ImageUrl = "https://placehold.co/600x401",
-                Occupancy = 4,
-                Price = 300,
-                Sqft = 550,
+                    Id = 2,
+                    Name = "Premium Pool Villa",
+                    Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    ImageUrl = "https://placehold.co/600x401",
+                    Occupancy = 4,
+                    Price = 300,
+                    Sqft = 550,
                 },
                 new Villa
                 {
-                Id = 3,
-                Name = "Luxury Pool Villa",
-                Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                ImageUrl = "https://placehold.co/600x402",
-                Occupancy = 4,
-                Price = 400,
-                Sqft = 750,
+                    Id = 3,
+                    Name = "Luxury Pool Villa",
+                    Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    ImageUrl = "https://placehold.co/600x402",
+                    Occupancy = 4,
+                    Price = 400,
+                    Sqft = 750,
                 }
             );
 
@@ -100,6 +101,69 @@ namespace WhiteLagoon.Infrastructure.Data
                 {
                     Villa_Number = 302,
                     VillaId = 3,
+                }
+            );
+
+            modelBuilder.Entity<Amenity>().HasData(
+                new Amenity
+                {
+                    Id = 1,
+                    VillaId = 1,
+                    Name = "Private Pool"
+                }, new Amenity
+                {
+                    Id = 2,
+                    VillaId = 1,
+                    Name = "Microwave"
+                }, new Amenity
+                {
+                    Id = 3,
+                    VillaId = 1,
+                    Name = "Private Balcony"
+                }, new Amenity
+                {
+                    Id = 4,
+                    VillaId = 1,
+                    Name = "1 king bed and 1 sofa bed"
+                },
+
+                new Amenity
+                {
+                    Id = 5,
+                    VillaId = 2,
+                    Name = "Private Plunge Pool"
+                }, new Amenity
+                {
+                    Id = 6,
+                    VillaId = 2,
+                    Name = "Microwave and Mini Refrigerator"
+                }, new Amenity
+                {
+                    Id = 7,
+                    VillaId = 2,
+                    Name = "Private Balcony"
+                }, new Amenity
+                {
+                    Id = 8,
+                    VillaId = 2,
+                    Name = "king bed or 2 double beds"
+                },
+
+                new Amenity
+                {
+                    Id = 9,
+                    VillaId = 3,
+                    Name = "Private Pool"
+                }, new Amenity
+                {
+                    Id = 10,
+                    VillaId = 3,
+                    Name = "Jacuzzi"
+                }, new Amenity
+                {
+                    Id = 11,
+                    VillaId = 3,
+                    Name = "Private Balcony"
                 }
             );
         }
