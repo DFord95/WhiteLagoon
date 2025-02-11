df25a332699758ec4c670c30d3d4324b7635bf61
Diontae Ford <forddt@dot.state.al.us>
2025-02-11 09:17:05 -0600

    Configuring Application Cookies

--- WhiteLagoon.Web/Program.cs
+++ WhiteLagoon.Web/Program.cs
@@ -20,6 +20,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.AccessDeniedPath = "/Account/AccessDenied";
+    options.LoginPath = "/Account/Login";
+});
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
